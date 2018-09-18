36d02662840e96a37fbe13ef17b3dc420774d1e6
Xiao Li <xiaoli.cbs@gmail.com>
2018-09-17 20:22:24 -0400

    need to finish

--- R/sampleDataset.R
+++ R/sampleDataset.R
@@ -1,7 +1,34 @@
+library(tools)
+
+.loadData <- function (input) {
+  if (is.data.frame(input)) {
+    df = input
+  } else if (is.character(input)) {
+    df = read_csv(input, sep='\t')
+  } else {
+    stop("Input is not a tsv file or a data.frame")
+  }
+  return(df)
+}
+
+.mergeMetr <- function(df, metrDf, primaryID) {
+  df = merge(df, metrDf, by = primaryID)
+  return(df)
+}
+
+.getNames <- function(df, primaryID) {
+  df = names(df)[-which(names(df) == primaryID)]
+}
+
 #' Sample dataset object
-#'
-#' Create a sample dataset object that contains all information needed to perform a sample
-#' quality control. The Dataset object is a list with one dataframe and
+#' @description Construct an Sample Dataset object
+#' A Sample Data Set object contains several attributes, including a data frame,
+#' which contains all the data for samples and several other attributes that
+#' contains metadata, such as qcMetrics, bamQcMetr, vcfQcMetr and PrimaryID.
+#' To check attributes of your current SDS
+#' Create a sample dataset object that contains all information needed to
+#' perform a sample quality control. The Dataset object is a list with one
+#' dataframe.
 #'
 #' @param bamQcMetr a data frame that contains BAM level QC metrics
 #' @param vcfQcMetr a data frame that contains VCF level QC metrics
@@ -9,37 +36,41 @@
 #' @param df optional, an R dataframe that contains all sample level data, including QC metrics and
 #'           sample annotations. When df is provided, bamQcMetr, vcfQcMetr and annotations should
 #'           be vectors with names of corresponding fields.
+#' @param stratify a scalar or vector containing annotations to stratify against
 #' @return an sampleDataset object.
 #' @export
 
 sampleDataset <- function(
-  bamQcMetr = NULL, vcfQcMetr = NULL, annotations, primaryID = NULL, df = NULL) {
-  if (is.null(primaryID))
+  annotInput, primaryID, bamQcInput=NULL, vcfQcInput=NULL, stratify=NULL,
+  df=NULL
+){
+  if (!exists(annotInput) || !exists(primaryID))
     stop("Primary ID of QC metrics and sample attributes should be provided.")
   if (is.null(bamQcMetr) & is.null(vcfQcMetr))
     stop("at least one set of QC metrics should be provided.")
-
-  if (is.null(df)) {
-    df = annotations
-    if (!is.null(bamQcMetr)) df = merge(df, bamQcMetr, by = primaryID)
-    if (!is.null(vcfQcMetr)) df = merge(df, vcfQcMetr, by = primaryID)
-    bamQcMetr = names(bamQcMetr)[-which(names(bamQcMetr) == primaryID)]
-    vcfQcMetr = names(vcfQcMetr)[-which(names(vcfQcMetr) == primaryID)]
-    annotations = names(annotations)[-which(names(annotations) == primaryID)]
-  } else {
-    if (is.data.frame(bamQcMetr))
-      stop("bamQcMetr should not be a data.frame when df is presented." )
-    if (is.data.frame(vcfQcMetr))
-      stop("vcfQcMetr should not be a data.frame when df is presented." )
-    if (is.data.frame(annotations))
-      stop("attributes should not be a data.frame when df is presented." )
+  # import annotation
+  df = .loadInput(annotInput)
+  annotNames = .getNames(df)
+  if (!all(stratify %in% annotNames)) {
+    stop("Not all stratified factors are in the annotation file,
+         please double check your input stratify option.")
   }
-  object = list(df = df, annotations = annotations,
-                qcMetrics = c(bamQcMetr, vcfQcMetr),
+
+  if (!is.null(bamQcInput)) {
+    bamQcMetr = .loadInput(bamQcInput)
+    df = mergeMetr(df, bamQcMetr, primaryID)
+    bamQcMetrName = .getNames(bamQcMetr)
+  }
+  if (!is.null(vcfQcInput)) {
+    vcfQcMetr = .loadInput(vcfQcInput)
+    df = .mergeMetr(df, vcfQcMetr, primaryID)
+    vcfQcMetrName = .getNames(vcfQcMetr)
+  }
+  sds = list(df = df, annotations = annotations,
+                qcMetrics = c(bamQcMetrName, vcfQcMetrName),
                 bamQcMetr = bamQcMetr, vcfQcMetr = vcfQcMetr,
                 primaryID = primaryID)
-  object$df$index = 1:length(df[[primaryID]])
-
+  sds$df$index = 1:length(df[[primaryID]])
   class(object) <- 'sampleDataset'
   return(object)
 }
--- exec/sampleQcTool.R
+++ exec/sampleQcTool.R
@@ -1,54 +1,14 @@
 #!/usr/bin/env Rscript
-library(devtools)
-install_github('xiaolicbs/SamplyzeR')
-library(optparse)
-library(SamplyzeR)
-.stratQcMetrHail <- function() {
-  return(c("nDeletion", "nInsertion", "nSNP", "rTiTv", "rHetHomVar", "rInsertionDeletion"))
+devtools::install_github('xiaolicbs/samplyzer')  # Stay current version
+library(argparse)
+library(samplyzer)
+
+createSds <- function (bamQcInput, vcfQcInput, annotInput, stratify, primaryID) {
+  stratify = unlist(strsplit(stratify, ',')
+  sds = sampleDataset(annotInput, primaryID, bamQcInput, vcfQcInput, stratify)
 }
 
-.stratQcMetrGATK <- function() {
-  return(c("nEvalVariants", "nInsertions", "nDeletions", "TiTvRatio", "InsDelRatio", "HetHomeRatio"))
-}
-option_list <- list(
-  make_option(c("-b", "--bamQcMetr"), help = "A tsv for BAM Level quality control metrics", default = NULL),
-  make_option(c("-v", "--vcfQcMetr"), help = "A tsv for VCF Level quality control metrics", default = NULL),
-  make_option(c("-a", "--annotations", help = "A tsv for sample annotation file")),
-  make_option(c("-d", "--primaryID"), help = "primary IDs used to specific each sample"),
-  make_option(c("-s", "--stratify"), help = "A list of attributes used in the stratified QC metrics analysis, separated by comma"),
-  make_option(c("-g", "--samplepc"), help = 'A tsv file with genotype PCs for each sample', default = NULL),
-  make_option(c("-r", "--refpc"), help = 'A tsv file with genotype PCs from a reference set of samples', default = NULL),
-  make_option(c("-p", "--prefix", help = "Prefix of output files")),
-  make_option(c("--cutoffs", help = 'a tsv file that contains hard cutoffs for QC metrics', default = NULL))
-)
-
-opt <- parse_args(OptionParser(option_list=option_list))
-
-if(!is.null(opt$bamQcMetr)) {
-  bamQcMetr = read.csv(opt$bamQcMetr, sep = '\t')
-} else {
-  bamQcMetr = NULL
-}
-if(!is.null(opt$vcfQcMetr)) {
-  vcfQcMetr = read.csv(opt$vcfQcMetr, sep = '\t')
-} else {
-  vcfQcMetr = NULL
-}
-annotations = read.csv(opt$annotations, sep = '\t')
-
-stratify = unlist(strsplit(opt$stratify, ','))
-
-if (!all(stratify %in% names(annotations))) {
-  stop("Not all stratified factors are in the annotation file, please double check your input stratify option.")
-}
-
-## Construct an Sample Dataset object
-## A Sample Data Set object contains several attributes, including a data frame, which contains all the data for samples and
-## several other attributes that contains metadata, such as qcMetrics, bamQcMetr, vcfQcMetr and PrimaryID. To check attributes of your current SDS
-sds = sampleDataset(bamQcMetr = bamQcMetr, vcfQcMetr = vcfQcMetr,
-                    annotations = annotations, primaryID = opt$primaryID)
-
-## Perform BAM sample QC
+# Perform BAM sample QC
 # flag samples with cutoffs
 if (is.null(opt$cutoff)) {
    cutoffs = data.frame(
@@ -61,34 +21,69 @@ if (is.null(opt$cutoff)) {
 } else {
    cutoffs = read.csv(opt$cutoffs, sep = '\t')
 }
-#sds = flagSamples(sds, cutoffs = cutoffs)
+sds = flagSamples(sds, cutoffs = cutoffs)
+
 # Save output to tables
 cat("\nSave output to RDS and write out sample info.\n")
 save(sds, RDS = paste(opt$prefix, 'RDS', sep = '.'),
      tsv = paste(opt$prefix, 'tsv', sep = '.'),
      xls = paste(opt$prefix, 'xls', sep = '.'))
+
 if(F) {
 #cat("Save BAM QC plot to pdf.\n")
 #grobList = sampleQcPlot(sds, qcMetrics = sds$bamQcMetr, annotation = stratify,
  #                         geom = 'scatter', ncols = 2, show = F)
-#ggplot2::ggsave(paste(opt$prefix, stratify,'QcPlot.pdf', sep = '.'), grobList, width = 12, height = 15)
+#ggplot2::ggsave(paste(opt$prefix, stratify,'QcPlot.pdf', sep = '.'), grobList,
+#  width = 12, height = 15)
 }
+
 if (!all(is.null(opt$refpc), is.null(opt$samplepc))) {
   cat(" o Load Sample and Reference genotype PCs\n")
   samplepc = read.csv(opt$samplepc, sep = '\t')
   refpc = read.csv(opt$refpc, sep = '\t')
 
   cat(" o Add genotype PCs to sample Dataset\n")
-  sds = setAttr(sds, attributes = 'PC', data = samplepc, primaryID = opt$primaryID)
+  sds = setAttr(sds, attributes = 'PC', data = samplepc,
+                primaryID = opt$primaryID)
 
   cat(" o Infer ancestry from PCA results\n")
-  sds = inferAncestry(sds, trainSet = refpc[,c('PC1', 'PC2', 'PC3')], knownAncestry = refpc$knownAncestry)
+  sds = inferAncestry(sds, trainSet = refpc[,c('PC1', 'PC2', 'PC3')],
+                      knownAncestry = refpc$knownAncestry)
 
   cat(" o Perform a stratified QC metrics analysis\n")
   ## Calculate z-scores for each batch across different ancestry
-  sds = calZscore(sds, strat=c(stratify,'inferredAncestry'), qcMetrics = .stratQcMetrGATK())
+  sds = calZscore(sds, strat=c(stratify,'inferredAncestry'),
+                  qcMetrics = .stratQcMetrGATK())
 
   cat(" o Filter samples with hard cutoffs and z-scores\n")
   sds = flagSamples(sds, cutoffs = cutoffs, zscore = 4)
 }
 cat(" o Done.")
+
+# mains script
+
+option_list <- list(
+  make_option(c("-b", "--bamQcTsv"), default = NULL,
+              help = "A tsv for BAM Level quality control metrics"),
+  make_option(c("--QcCutoffs", default = NULL,
+                help = 'a tsv file that contains hard cutoffs for QC metrics')),
+  make_option(c("-v", "--vcfQcTsv"), default = NULL,
+              help = "A tsv for VCF Level quality control metrics"),
+  make_option(c("-a", "--annotTsv",
+                help = "A tsv for sample annotation file")),
+  make_option(c("-d", "--primaryID"),
+              help = "primary IDs used to specific each sample"),
+  make_option(c("-s", "--stratify"),
+              help = "A list of attributes used in the stratified QC metrics
+              analysis, separated by comma"),
+  make_option(c("-g", "--samplePc"), default = NULL,
+              help = 'A tsv file with genotype PCs for each sample'),
+  make_option(c("-r", "--refPc"), default = NULL,
+              help = 'A tsv file with genotype PCs from a reference set of
+              samples'),
+  make_option(c("-p", "--prefix", help = "Prefix of output files"))
+  )
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+sampleQc()
