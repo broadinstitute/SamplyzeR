e7e0a908733ee4f78279fe0594f9c90bd2c4be85
admin <1677953017@qq.com>
2023-11-15 16:28:16 +0800

    MarkDown

--- R/inferAncestry.R
+++ R/inferAncestry.R
@@ -15,7 +15,7 @@ inferAncestry <- function(...) UseMethod('inferAncestry')
 #' @export
 
 inferAncestry.default <- function(testSet, trainSet, ancestry, k = 5) {
-  inferredAncestry = class::knn(train = trainSet, test = testSet, cl = ancestry, k = k)
+  inferredAncestry <- class::knn(train = trainSet, test = testSet, cl = ancestry, k = k)
   return(inferredAncestry)
 }
 
@@ -32,22 +32,25 @@ inferAncestry.sampleDataset <- function(
   sds, trainSet, knownAncestry, nPC = 3, k = 5
 ) {
   if(dim(trainSet)[1] != length(knownAncestry)) stop("Number of rows of training set is different from knownAncestry.")
+  if (nPC > ncol(trainSet)) {
+    stop("nPC exceeds the number of available principal components in trainSet.")
+  }
 
-  PC = paste('PC', 1:nPC, sep = '')
+  PC <- paste('PC', 1:nPC, sep = '')
   if(!all(PC %in% names(trainSet))) stop("Train Set does not contain nPC specified for the analysis.")
   if(!all(PC %in% names(sds$df[sds$PC]))) stop("Test Set does not contain nPC specified for this analysis.")
 
-  trainSet = trainSet[, PC]
-  testSet = sds$df[, PC]
+  trainSet <- trainSet[, PC]
+  testSet <- sds$df[, PC]
 
-  inferredAncestry = inferAncestry(testSet, trainSet, ancestry = knownAncestry, k = k)
+  inferredAncestry <- inferAncestry(testSet, trainSet, ancestry = knownAncestry, k = k)
   if (exists("isTrainSet")) {
-    sds$df$inferredAncestry = as.character(sds$df$knownAncestry)
-    sds$df[!isTrainSet, 'inferredAncestry'] = as.character(inferredAncestry)
+    sds$df$inferredAncestry <- as.character(sds$df$knownAncestry)
+    sds$df[!isTrainSet, 'inferredAncestry'] <- as.character(inferredAncestry)
   } else {
-    sds$df$inferredAncestry = as.character(inferredAncestry)
+    sds$df$inferredAncestry <- as.character(inferredAncestry)
   }
-  sds$inferredAncestry = 'inferredAncestry'
-  sds$annot = c(sds$annot, 'inferredAncestry')
+  sds$inferredAncestry <- 'inferredAncestry'
+  sds$annot <- c(sds$annot, 'inferredAncestry')
   return(sds)
 }
