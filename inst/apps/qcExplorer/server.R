1e258196977b202e19bf2aaa3f165480726524c3
admin <1677953017@qq.com>
2023-11-17 14:58:13 +0800

    feat/28-date_upload_tab_page Fixes #28

--- inst/apps/qcExplorer/server.R
+++ inst/apps/qcExplorer/server.R
@@ -1,39 +1,88 @@
-server <- function(input, output) {
-  values <- reactiveValues(df_data = NULL, outliers = NULL)
-  observeEvent(input$plot_brush1, {
-    values$df_data = subsetTable(input, sds, 1)
-    values$outliers = values$df_data$sampleId
+# Server
+server <- shinyServer(function(input, output, session) {
+  file_data <- reactiveValues(
+    bamQcMetr = NULL,
+    annotations = NULL,
+    vcfQcMetr = NULL,
+    samplePc = NULL,
+    refpc = NULL,
+    sds = NULL
+  )
+  reactive_sds <- reactive({
+    file_data$sds()
   })
 
-  # QC scatterplot
-  output$plot1 <- renderPlot({
-    plot(sampleQcPlot(
-      sds, annotation = input$anno1, qcMetrics = input$qcMetrics1,
-      geom='scatter' #, outliers = input$outliers
-    ))
+  selectedAnno <- reactive({
+    input$anno1
   })
 
-  # QC violin plot
-  output$violin <- renderPlot({
-    plot(sampleQcPlot(sds, qcMetrics = input$qcMetrics1, geom = 'violin',
-                      annotation = input$anno1)) # outliers = input$outliers
+  selectedQcMetrics <- reactive({
+    input$qcMetr1
   })
 
-  # QC metrics correlation
-  output$qcCorr <- renderPlot({
-    samplyzer::scatter(
-      data = subsetTable(input, sds, 1), x = input$qcMetr1, y = input$qcMetr2,
-      strat = input$attr, primaryID = sds$primaryID)
+  observeEvent(c(input$bamQcMetrFile, input$annotationsFile, input$vcfQcMetrFile), {
+    # Check if all the required files are provided
+    if (!is.null(input$bamQcMetrFile) && !is.null(input$annotationsFile) && !is.null(input$vcfQcMetrFile)) {
+      # Check if optional files are provided
+      if (!is.null(input$samplePCsFile)) {
+        file_data$samplePc <- read.csv(input$samplePCsFile$datapath, sep = '\t')
+      }
+      if (!is.null(input$refPCsFile)) {
+        file_data$refPC <- read.csv(input$refPCsFile$datapath, sep = '\t')
+      }
+      file_data$bamQcMetr <- read.csv(input$bamQcMetrFile$datapath, sep = '\t', row.names = NULL)
+      file_data$annotations <- read.csv(input$annotationsFile$datapath, sep = '\t', row.names = NULL)
+      file_data$vcfQcMetr <- read.csv(input$vcfQcMetrFile$datapath, sep = '\t', row.names = NULL)
+
+
+      file_data$sds <- sampleDataset(
+        bamQcInput = file_data$bamQcMetr,
+        vcfQcInput = file_data$vcfQcMetr,
+        annotInput = file_data$annotations,
+        primaryID = 'SampleID'
+      )
+      updateSelectInput(session, "qcMetr1", choices = unique(file_data$sds$qcMetrics))
+      updateSelectInput(session, "qcMetr2", choices = unique(file_data$sds$qcMetrics))
+      updateSelectInput(session, "anno1", choices = unique(file_data$sds$annot))
+
+      # QC scatterplot
+      output$plot1 <- renderPlot({
+        sampleQcPlot(
+          file_data$sds, annot= selectedAnno(), qcMetrics = selectedQcMetrics(),
+          geom= "scatter", outliers = input$outliers, show = T)
+      })
+
+      # QC violin plot
+      output$plot2 <- renderPlot({
+        sampleQcPlot(
+          file_data$sds, annot= selectedAnno(), qcMetrics = selectedQcMetrics(),
+          geom= "violin", outliers = input$outliers, show = T)
+      })
+      file_data$sds = setAttr(file_data$sds, attributes = 'PC', data = file_data$samplePc, primaryID = 'SampleID')
+      file_data$sds <- inferAncestry(
+        file_data$sds,
+        trainSet = file_data$refPC[, grep("^PC", names(file_data$refPC))],
+        knownAncestry =  file_data$refPC$group,
+      )
+
+      # pca correlation
+      output$pca <- renderPlot({
+        samplyzer:::scatter(
+          data = file_data$sds$df, x = input$PCx, y = input$PCy, strat = input$anno1,
+          outliers = input$outliers, primaryID = file_data$sds$primaryID)
+      })
+
+      observeEvent(input$plot_brush1, {
+        sds_subset = subsetTable(input, file_data$sds, 1)
+        # QC metrics correlation
+        output$qcCorr <- renderPlot({
+          samplyzer:::scatter(
+            data = sds_subset, x = input$qcMetr1, y = input$qcMetr2, strat = input$anno1,
+            outliers = input$outliers, primaryID = file_data$sds$primaryID
+          )})
+
+        output$table1 <- renderTable(sds_subset)
+      })
+    }
   })
-
-  # pca correlation
-  output$pca <- renderPlot({
-    samplyzer::scatter(
-      data = sds$df, x = input$PCx, y = input$PCy, strat = input$attr2,
-      outliers = input$outliers, primaryID = sds$primaryID)
-  })
-
-  # render outlier info
-  output$table1 <- renderTable(values$df_data)
-}
-
+})
